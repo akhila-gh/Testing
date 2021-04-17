04a8eed0b7852162f4281d52b8d2689130c66ccf
Akhila <akhilashastry1698@gmail.com>
2021-04-17 10:53:41 +0530

    App committed

---
+++ app.js
@@ -0,0 +1,35 @@
+for( let i=1; i<=10;i++)
+{
+    console.log(i);
+}
+
+let array=["javascript","java","phython","nodejs","reactjs"];
+ for( let i of array)
+ {
+     console.log(i);
+ }
+
+
+
+
+/*let mark,percentage,index;
+mark=[65,45,89,56];
+if(mark[0]<35 || mark[1]<35||mark[2]<35||mark[3]<35)
+{
+    console.log("failed");
+}
+else{
+percentage=((mark[0]+mark[1]+mark[2]+mark[3])/400)*100;
+}
+if(percentage>75)
+{
+    console.log("A");
+}
+else if(percentage>60)
+{
+    console.log("B");
+}
+else
+{
+ console.log("C");
+}
